338af782271eb4a2373a62aa4e3eb5867659de5a
Minsung Kim <minsungk@c5r3s5.42seoul.kr>
2021-02-18 21:36:23 +0900

    fix buffer size

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: minsungk <minsungk@stduent.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/12 22:09:18 by minsungk          #+#    #+#             */
-/*   Updated: 2021/02/18 17:16:40 by minsungk         ###   ########.fr       */
+/*   Updated: 2021/02/18 21:33:57 by minsungk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,16 +28,21 @@ int		have_newline(const char *backupfd)
 
 int		split_newline(char **backup, char **line, size_t new_idx)
 {
-	size_t temp;
-	char *temp1;
-	temp = ft_strlen(*backup);
-	(*backup)[new_idx] = '\0';
-	free(*backup);
-	*backup = *backup + new_idx;
-	
-	*line = ft_strdup(*backup);
+	size_t	len;
+	char	*temp;
 
-	*backup = *backup
+	(*backup)[new_idx] = '\0';
+	*line = ft_strdup(*backup);
+	len = ft_strlen(*backup + new_idx + 1);
+	if (len == 0)
+	{
+		*backup = 0;
+		return (1);
+	}
+	temp = ft_strdup(*backup + new_idx + 1);
+	free(*backup);
+	*backup = temp;
+	return (1);
 }
 
 int		get_next_line(int fd, char **line)
@@ -48,14 +53,13 @@ int		get_next_line(int fd, char **line)
 	size_t			new_idx;
 
 	if (fd < 0 || !line || fd >= OPEN_MAX || BUFFER_SIZE <= 0)
-		return (-1);
+		return (ERROR);
 	while (0 < (rd_size = read(fd, (char *)buff, BUFFER_SIZE - 1)))
 	{
-		buff[rd_size] = '\0';
+		// buff[rd_size] = '\0';
 		backup[fd] = ft_strjoin(backup[fd], buff);
 		if ((new_idx = have_newline(backup[fd])) >= 0)
 			return (split_newline(&backup[fd], line, new_idx));
 	}
-	return ()
+	return ();
 }
-
