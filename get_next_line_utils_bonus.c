3a1eb768a334f3e09abf63c39028db4f8ec7297c
minsungk <minsungk@student.42seoul.kr>
2021-03-04 00:26:39 +0900

    fix error

--- get_next_line_utils_bonus.c
+++ get_next_line_utils_bonus.c
@@ -42,8 +42,7 @@ char	*ft_strjoin(char const *s1, char const *s2)
 
 	if (!s1 || !s2)
 		return (NULL);
-	if (!(temp = (char *)malloc(sizeof(char) * (len1 + len2 + 1))))
-		return (NULL);
+	temp = (char *)malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1));
 	if (temp)
 	{
 		i = 0;
